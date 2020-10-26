1219d8aa45592c5dc8c571872e271bf91ba61bcf
tsega2130 <69941454+tsega2130@users.noreply.github.com>
2020-10-25 21:04:35 -0400

    objects

---
+++ objects.js
@@ -0,0 +1,20 @@
+let personData={
+    firstName: 'Jean-Luc',
+    lastName:'Picard',
+    age: 79
+} 
+
+let daysInMonth={
+    'January': 31,
+    'February': 28/29,
+    'March': 31,
+    'April': 30,
+    'May': 31,
+    'June': 30,
+    'July': 31,
+    'August': 31,
+    'September': 30,
+    'October': 31,
+    'November': 30,
+    'December':31
+}
